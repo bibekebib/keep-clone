31658113434eac424578203e4e8e0a37ba5d856f
bibekebib <42284727+bibekebib@users.noreply.github.com>
2020-08-24 17:23:56 +0545

    Updates

---
+++ precache-manifest.d9ba407c610d9c9fdee13133d674d1c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c920635d7c536168da673aa3ace6c0b",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "a1916278bb7553606828",
+    "url": "/my-app/static/css/main.7718d9ca.chunk.css"
+  },
+  {
+    "revision": "e7eb999a7519dac103b6",
+    "url": "/my-app/static/js/2.2a118290.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.2a118290.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1916278bb7553606828",
+    "url": "/my-app/static/js/main.2c777697.chunk.js"
+  },
+  {
+    "revision": "24e2d749d0a9d71883d3",
+    "url": "/my-app/static/js/runtime-main.9508bf7c.js"
+  }
+]);
